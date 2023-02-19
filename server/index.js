0aa1c48c85bc4d53c426c5a1c6d93a183b961620
AlinaZaharia <zalinutza@yahoo.com>
2023-02-19 16:06:43 +0200

    mesaj

--- server/index.js
+++ server/index.js
@@ -1,29 +1,45 @@
-const express = require('express');
+const express = require("express");
+const fileUpload = require('express-fileupload');
 const PORT = process.env.PORT || 3001;
 const app = express();
 const fs = require('fs');
 const cors = require('cors');
-//const citesteFis = require('./citesteFis');
 
-async function citesteFis(req, res, next) {
-  const dirname = './fisiere/originale/';
-  const rezultat = [];
+const dirname = './fisiere/originale/';
 
-  const listaFisiere = await fs.promises.readdir(dirname);
+async function citesteFis(req, res, next){
+    const rezultat = [];
 
-  for (const file of listaFisiere) {
-    const continutFisier = await fs.promises.readFile(dirname + file, 'utf-8');
-    rezultat.push({ numeFis: file, text: continutFisier });
-  }
+    const listaFisiere = await fs.promises.readdir(dirname);
+    
+    for(const file of listaFisiere){
+        const continutFisier = await fs.promises.readFile(dirname+file, 'utf-8');
+        rezultat.push({numeFis:file, text:continutFisier});
+    }
 
-  req.rezultat = rezultat;
-  next();
+    req.rezultat=rezultat;
+    next();
 }
+
+function descarcaFisier(fisier){
+    fs.writeFile(dirname + fisier.name, fisier.data, (err)=>{console.log(err)} );
+}
+
 app.use(cors());
-app.get('/fisiere', citesteFis, (req, res) => {
-  res.json([...req.rezultat]);
+
+app.get("/fisiere", citesteFis, (req, res) => {
+    res.json([...req.rezultat]);
+});
+
+app.post("/fisiere", 
+    fileUpload({ createParentPath: true })
+    , (req, res) => {
+        descarcaFisier(req.files.fisier);
+        return res.json();
 });
 
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+    console.log(`Server listening on ${PORT}`);
 });
+
+
